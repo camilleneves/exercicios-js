88350c86997d9aabec5c70c9491f2aaa6064edf5
CAMILLE BRUNES NEVES <45657999878@info.local>
2024-05-09 09:57:46 -0300

    feat: ex2 terminado

---
+++ ex2.js
@@ -0,0 +1,29 @@
+//Exercício 2: Verificação de dia da semana
+//Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+//semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+//2 representa terça-feira e assim por diante.
+
+let numeroDia = prompt("Digite um número de 1 a 7 representando um dia dasemana: ")
+
+if (numeroDia == 1) {
+    console.log("Segunda-Feira")
+}
+if (numeroDia == 2) {
+    console.log("Terca-Feira")
+}
+if (numeroDia == 3) {
+    console.log("Quarta-Feira")
+}
+if (numeroDia == 4) {
+    console.log("Quinta-Feira")
+}
+if (numeroDia == 5) {
+    console.log("Sexta-Feira")
+}
+if (numeroDia == 6) {
+    console.log("Sabado")
+}
+if (numeroDia == 7) {
+    console.log("Domingo")
+}
+
