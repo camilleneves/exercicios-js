9303ce363115858ce9b8855df7a76b5741b53716
CAMILLE BRUNES NEVES <45657999878@info.local>
2024-05-09 10:53:44 -0300

    feat:ex4

--- ex4.js
+++ ex4.js
@@ -2,3 +2,21 @@
 //Faça um programa que receba 4 notas de um aluno, calcule e imprima a média aritmética das
 //notas e a mensagem de APROVADO para média superior ou igual a 7,0 RECUPERAÇÃO para
 //notas entre 5.0 e 7,0 ou a mensagem de REPROVADO para média inferior a 5,0.
+
+let nota1 = prompt("Digite a nota 1: ")
+let nota2 = prompt("Digite a nota 2: ")
+let nota3 = prompt("Digite a nota 3: ")
+let nota4 = prompt("Digite a nota 4: ")
+
+let media = (nota1 + nota2 + nota3 + nota4 / 4)
+
+
+if (media >= 7.0) {
+    alert("APROVADO");
+} 
+else if (media >= 5.0) {
+    alert("RECUPERAÇÃO");
+} 
+else {
+        alert("REPROVADO");
+}
