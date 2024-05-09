1da834dbcc95992c877b2ebb9cfd12dd048bb70e
CAMILLE BRUNES NEVES <45657999878@info.local>
2024-05-09 08:44:09 -0300

    feat: exercicio 1

---
+++ ex1.js
@@ -0,0 +1,22 @@
+//Exercício 1: Verificação de login
+//Escreva um programa que solicita ao usuário um nome de usuário e uma senha.
+//Verifique se o nome de usuário é "admin"; e a senha é "senha123";. 
+//Exiba uma mensagem indicando se o login foi bem-sucedido ou não.
+
+//Solicita ao usuário um nome de usuário 
+let username = prompt("Digite um nome de usuário: ");
+
+//Solicita ao usuário uma senha
+let senha = prompt("Digite a senha: ");
+
+//Verifique se o nome de usuário é "admin"; e a senha é "senha123"
+//CASO SIM -> Exiba a mensagem de login bem sucedida.
+//CASO NAO -> Exiba a mensagem de usuario ou senha incorreta.
+
+if (username == "admin" && senha == "senha123"){
+    console.log("Login bem sucedido")
+} 
+
+else{
+    console.log ("Login Incorreto")
+}
---
+++ exemplo.js
@@ -0,0 +1,28 @@
+//Apresentar mensagem na tela//
+//alert("Hello World!!")
+//console.log("Hello World!!");
+
+//Escreva um programa que solicite o nome do usuario
+// e lhe de boas vindas, exibindo uma mensagem pelo console.
+
+//prompt() -> recebe uma entrada digitada
+//let nome = prompt("Ola, digite seu nome: ");
+
+//Concatenação -> juntar texto
+//console.log("Seja bem vindo " + nome + "!!!")
+
+//let pontos = 100;
+
+//parseInt -> Transforma o que esta entre () em numero inteiro
+
+//let bonus = parseInt(prompt("Quantos pontos voce deseja somar??"))
+
+//let totalPontos = pontos + bonus;
+
+//console.log(totalPontos);
+
+//let notaAluno = prompt("Digite a nota do aluno: ")
+
+//console.log(notaAluno)
+
+//parseFload -> Transforma o que esta enter () em numero com virgula / numero fracionado
