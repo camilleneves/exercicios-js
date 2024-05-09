0c1332052d990812975631244a3551d04f8b69b1
CAMILLE BRUNES NEVES <45657999878@info.local>
2024-05-09 10:04:48 -0300

    feat: ex 3 Terminado

---
+++ ex3.js
@@ -0,0 +1,12 @@
+//Exercício 3: Ímpar ou par
+//Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let numero = prompt("Digite um numero: ")
+
+if(numero == 1,3,5,7,9) {
+    console.log("Impar")
+}
+
+if(numero == 2,4,6,8) {
+    console.log("Par")
+}
